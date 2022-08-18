ed99f0a22c43966378412b8e7f746d648bf13f4d
Robin Kåveland <kaaveland@gmail.com>
2022-08-18 15:26:52 +0200

    Add API for utslipp sector structure

--- utslippsregnskap-api/app.py
+++ utslippsregnskap-api/app.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, jsonify
 import pandas as pd
+import itertools
 
 app = Flask("utslippsregnskap")
 app.config.update(
@@ -11,7 +12,7 @@ app.config.update(
 
 df = pd.read_parquet("../data/utslippsdata_med_type.pq")
 df = df.astype({"versjon": "category", "type": "category", "enhet": "category"})
-
+nivaa_navn = df.loc[:, df.columns[df.columns.str.endswith('navn')]].drop_duplicates()
 
 @app.after_request
 def security_headers(response):
@@ -32,6 +33,32 @@ def utslipp_jordbruk():
         .to_dict(orient="list")
     )
     return next_df
+
+
+@app.get("/utslipp/nivaa")
+def utslipp_nivaa_navn():
+    out = {}
+    for r in nivaa_navn.to_dict(orient='records'):
+        modify = out
+        for column, value in r.items():
+            if not isinstance(value, str):
+                break
+            if value not in modify:
+                modify[value] = {}
+            modify = modify[value]
+    def pretty(tree):
+        values = []
+        for key, children in tree.items():
+            if not children:
+                values.append({ 'kategori': key })
+            else:
+                values.append({
+                    'kategori': key,
+                    'nivaa': pretty(children)
+                })
+        return values
+    return jsonify(pretty(out))
+
 
 if __name__ == '__main__':
     app.run(debug=True)

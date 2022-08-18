813ab9a74eb8e5dadb37423a9d56dddd0198a8a2
Robin Kåveland <kaaveland@gmail.com>
2022-08-18 11:57:13 +0200

    Set some reasonable security headers for flask

--- utslippsregnskap-api/app.py
+++ utslippsregnskap-api/app.py
@@ -1,28 +1,34 @@
 from flask import Flask
 import pandas as pd
 
-app = Flask('utslippsregnskap')
-
-df = pd.read_parquet('../data/utslippsdata_med_type.pq')
-df = df.astype(
-    {'versjon': 'category', 'type': 'category', 'enhet': 'category'}
+app = Flask("utslippsregnskap")
+app.config.update(
+    SESSION_COOKIE_SECURE=True,
+    SESSION_COOKIE_HTTPONLY=True,
+    SESSION_COOKIE_SAMESITE="Lax",
 )
 
+
+df = pd.read_parquet("../data/utslippsdata_med_type.pq")
+df = df.astype({"versjon": "category", "type": "category", "enhet": "category"})
+
+
 @app.after_request
-def allow_cors(response):
-    print('response', response)
-    response.headers.access_control_allow_origin = '*'
+def security_headers(response):
+    response.headers.access_control_allow_origin = "*"
+    response.headers["Strict-Transport-Security"] = "max-age=31536000; includeSubDomains"
+    response.headers["Content-Security-Policy"] = "default-src 'self'"
+    response.headers["X-Content-Type-Options"] = "nosniff"
+    response.headers["X-Frame-Options"] = "SAMEORIGIN"
+    return response
+
 
 @app.get("/utslipp/jordbruk")
 def utslipp_jordbruk():
-    next_df = df.loc[
-        (df.norskkilde_nivaa1_navn == 'Jordbruk') &
-        (df.versjon == '2019-11-01') &
-        (df.type == 'POP')
-    ].groupby(
-        ['aar', 'versjon'],
-        as_index=False,
-        observed=True
-    ).agg({ 'utslipp': sum }).to_dict()
+    next_df = (
+        df.loc[(df.norskkilde_nivaa1_navn == "Jordbruk") & (df.versjon == "2019-11-01") & (df.type == "POP")]
+        .groupby(["aar", "versjon"], as_index=False, observed=True)
+        .agg({"utslipp": sum})
+        .to_dict()
+    )
     return next_df
-

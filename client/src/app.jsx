0a56c4b0f348933130e492b02df17ec2b091f4a8
erlendmiljo <erlend.vige@miljodir.no>
2022-08-18 17:25:14 +0200

    Render checkboxes from nivaaer list

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import { render } from "react-dom";
 import Plot from "react-plotly.js";
 import "@miljodirektoratet/md-css";
-import { MdButton } from "@miljodirektoratet/md-react";
+import { MdButton, MdCheckbox, MdChecklist } from "@miljodirektoratet/md-react";
 
 const App = () => {
   const [isLoading, setIsLoading] = React.useState(true);
   const [data, setData] = React.useState([]);
+  const [allChecked, setAllChecked] = React.useState(false);
+  const [nivaaer, setNivaaer] = React.useState([]);
 
   React.useEffect(() => {
     async function fetchData() {
@@ -18,11 +20,42 @@ const App = () => {
     fetchData();
   }, []);
 
+  React.useEffect(() => {
+    async function fetchData() {
+      const response = await fetch("http://localhost:5000/utslipp/nivaa");
+      const data = await response.json();
+      setNivaaer(data);
+    }
+    fetchData();
+  }, [isLoading]);
+
   if (isLoading) {
     return <p>Henter data</p>;
   } else if (data.length === 0) {
     return <p>Har ikke fått data</p>;
   }
+
+  const renderCheckboxes = (nivå) => {
+    if (!nivå.nivaa) {
+      return (
+        <MdCheckbox
+          label={nivå.kategori}
+          checked={allChecked}
+          onChange={() => setAllChecked(!allChecked)}
+        />
+      );
+    } else {
+      return (
+        <MdChecklist
+          label={nivå.kategori}
+          checked={allChecked}
+          onChange={() => setAllChecked(!allChecked)}
+        >
+          {nivå.nivaa.map(renderCheckboxes)}
+        </MdChecklist>
+      );
+    }
+  };
 
   return (
     <div>
@@ -37,6 +70,8 @@ const App = () => {
           },
         ]}
       ></Plot>
+      {JSON.stringify(nivaaer)}
+      {nivaaer.map(renderCheckboxes)}
     </div>
   );
 };
